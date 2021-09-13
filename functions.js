43b2cd39690a1887733fbf3cf8196dabaf122f0e
ionut ch <ionut.chisiu@yahoo.com>
2021-09-13 20:27:51 +0300

    fix highlight

--- functions.js
+++ functions.js
@@ -2,22 +2,28 @@ let allTeams = [];
 let editId;
 
 function loadTeams() {
-  fetch("http://localhost:3000/teams-json")
-    .then(r => r.json())
-    .then(teams => {
-      console.warn('teams', teams);
+  fetch("http://localhost:3000/teams")
+    .then((r) => r.json())
+    .then((teams) => {
+      console.warn("teams", teams);
       allTeams = teams;
       displayTeams(teams);
-    })
+    });
 }
 
-function getTeamsAsHTML(teams) {
+function highlight(text, search) {
+  return search ? text.replaceAll(new RegExp(search, "gi"), m => {
+    return `<span class="highlight">${m}</span>` ;
+  }) : text;
+}
+
+function getTeamsAsHTML(teams, search) {
   return teams.map(team => {
     return `<tr>
-        <td>${team.promotion}</td>
-        <td>${team.members}</td>
-        <td>${team.name}</td>
-        <td>${team.url}</td>
+        <td>${highlight(team.promotion, search)}</td>
+        <td>${highlight(team.members, search)}</td>
+        <td>${highlight(team.name, search)}</td>
+        <td>${highlight(team.url, search)}</td>
         <td>
           <a href="#" class="delete-btn" data-id="${team.id}">&#10006;</a>
           <a href="#" class="edit-btn" data-id="${team.id}">&#9998;</a>
@@ -27,60 +33,61 @@ function getTeamsAsHTML(teams) {
 };
 
 function displayTeams(teams) {
-  const html = getTeamsAsHTML(teams);
+  const search = document.getElementById("search").value.toLowerCase();
+  const html = getTeamsAsHTML(teams, search);
 
   document.querySelector('#list tbody').innerHTML = html;
 }
 
 function getTeamValues() {
-  const promotion = document.querySelector('[name=promotion]').value;
-  const members = document.querySelector('[name=members]').value;
-  const name = document.querySelector('[name=name]').value;
-  const url = document.querySelector('[name=url]').value;
+  const promotion = document.querySelector("[name=promotion]").value;
+  const members = document.querySelector("[name=members]").value;
+  const name = document.querySelector("[name=name]").value;
+  const url = document.querySelector("[name=url]").value;
 
   return {
     promotion: promotion,
     members: members,
     name,
-    url
+    url,
   };
 }
 
 function setTeamValues(team) {
-  console.warn('edit', team);
-  document.querySelector('[name=promotion]').value = team.promotion;
-  document.querySelector('[name=members]').value = team.members;
-  document.querySelector('[name=name]').value = team.name;
-  document.querySelector('[name=url]').value = team.url;
+  console.warn("edit", team);
+  document.querySelector("[name=promotion]").value = team.promotion;
+  document.querySelector("[name=members]").value = team.members;
+  document.querySelector("[name=name]").value = team.name;
+  document.querySelector("[name=url]").value = team.url;
 }
 
 function saveTeam(team) {
-  fetch("http://localhost:3000/teams-json/create", {
+  fetch("http://localhost:3000/teams/create", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(team)
+    body: JSON.stringify(team),
   })
-    .then(r => r.json())
-    .then(status => {
+    .then((r) => r.json())
+    .then((status) => {
       if (status.success) {
         loadTeams();
-        document.querySelector('form').reset();
+        document.querySelector("form").reset();
       }
     });
 }
 
 function deleteTeam(id) {
-  fetch("http://localhost:3000/teams-json/delete", {
+  fetch("http://localhost:3000/teams/delete", {
     method: "DELETE",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({ id })
+    body: JSON.stringify({ id }),
   })
-    .then(r => r.json())
-    .then(status => {
+    .then((r) => r.json())
+    .then((status) => {
       if (status.success) {
         loadTeams();
       }
@@ -88,18 +95,18 @@ function deleteTeam(id) {
 }
 
 function updateTeam(team) {
-  fetch("http://localhost:3000/teams-json/update", {
+  fetch("http://localhost:3000/teams/update", {
     method: "PUT",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(team)
+    body: JSON.stringify(team),
   })
-    .then(r => r.json())
-    .then(status => {
+    .then((r) => r.json())
+    .then((status) => {
       if (status.success) {
         loadTeams();
-        document.querySelector('form').reset();
+        document.querySelector("form").reset();
         editId = 0;
       }
     });
@@ -107,7 +114,7 @@ function updateTeam(team) {
 
 function editTeam(id) {
   editId = id;
-  const team = allTeams.find(team => team.id === id);
+  const team = allTeams.find((team) => team.id === id);
   setTeamValues(team);
 }
 
@@ -123,7 +130,7 @@ function submitTeam() {
 
 loadTeams();
 
-document.querySelector('#list tbody').addEventListener("click", e => {
+document.querySelector("#list tbody").addEventListener("click", (e) => {
   if (e.target.matches("a.delete-btn")) {
     const id = e.target.getAttribute("data-id");
     deleteTeam(id);
@@ -133,13 +140,15 @@ document.querySelector('#list tbody').addEventListener("click", e => {
   }
 });
 
-document.getElementById("search").addEventListener("input", e => {
+document.getElementById("search").addEventListener("input", (e) => {
   const text = e.target.value.toLowerCase();
-  const filtered = allTeams.filter(team => {
-    return team.members.toLowerCase().includes(text) ||
-    team.name.toLowerCase().includes(text)  ||
-    team.promotion.toLowerCase().includes(text)||
-    team.url.toLowerCase().includes(text);
+  const filtered = allTeams.filter((team) => {
+    return (
+      team.members.toLowerCase().includes(text) ||
+      team.name.toLowerCase().includes(text) ||
+      team.promotion.toLowerCase().includes(text) ||
+      team.url.toLowerCase().includes(text)
+    );
   });
   displayTeams(filtered);
 });
